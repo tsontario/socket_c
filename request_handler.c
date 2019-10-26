77e1c7d15cfac9914fa433a42befa24793caff29
Timothy Smith <tsontario@gmail.com>
2019-10-26 19:36:24 -0400

    server happy-path is working. TODO failure modes

--- request_handler.c
+++ request_handler.c
@@ -52,12 +52,18 @@ int handle_conn(int client_sock)
     return 1;
   }
 
-  if ((serve_request(client_sock, &request, &response)) == -1)
+  int response_code;
+  if ((response_code = serve_request(client_sock, &request, &response)) != 0)
   {
-    // TODO serve 500
-    return 1;
+    if (response_code >= 400)
+    {
+      write_http_error(client_sock, response_code);
+      return 1;
+    } else {
+      printf("error in serverequest");
+      return 1;
+    }
   }
-  
   if (close(client_sock) == -1)
   {
     perror("error closing socket");
@@ -74,27 +80,27 @@ int parse_http_req(char* buf, size_t buf_len, http_req* req)
   if (req_fd == NULL)
   {
     perror("fmemopen parse_http_req");
-    return 1;
+    return 500;
   }
 
   // Get the HTTP start line and parse into req
   if (parse_start_line(req_fd, req) == -1)
   {
     printf("error parsing start line\n");
-    return 1;
+    return 422;
   }
 
   if (parse_headers(req_fd, req) == -1)
   {
     printf("error parsing headers\n");
-    return 1;
+    return 422;
   }
 
   // All that remains is to read the body
   if (parse_body(req_fd, req) == -1)
   {
     printf("error parsing request body\n");
-    return 1;
+    return 422;
   }
 
   printf("> New Request:\n>\t%s %s %s\n", req->verb, req->path, req->version);
@@ -103,6 +109,130 @@ int parse_http_req(char* buf, size_t buf_len, http_req* req)
   printf(">\n%s\n\n", req->body);
 
   return 0;
+}
+
+// serve_request serves the request specified by req, using resp 
+int serve_request(int client_sock, http_req* req, http_resp* resp)
+{
+  char* local_path = (char*)malloc(strlen(WEB_DIR) + strlen(req->path) + 1); // +1 for \0 byte
+  strncat(local_path, WEB_DIR, strlen(WEB_DIR));
+  strncat(local_path, req->path, strlen(req->path));
+  // Check path exists
+  struct stat st;
+  if ((stat(local_path, &st)) == -1)
+  {
+    // For simplicity we assume any failure is ENOENT and we'll return 404
+    perror("error calling stat on request path");
+    free(local_path);
+    return 404;
+  }
+  resp->body_fd = fopen(local_path, "r");
+  if (resp->body_fd == NULL)
+  {
+    perror("error opening requested file");
+    free(local_path);
+    return 500;
+  }
+  free(local_path);
+  fseek(resp->body_fd, 0, SEEK_END);
+  long content_length = ftell(resp->body_fd);
+  fseek(resp->body_fd, -content_length, SEEK_END);
+  char* buf = (char*)malloc(sizeof(char)*BUF_SIZE);
+  memset(buf, 0, BUF_SIZE);
+
+  // We are ready to send
+  resp->status_code = 200;
+  header_list* header = (header_list*)malloc(sizeof(header_list));
+  resp->headers = header;
+  header->entry = (header_entry*)malloc(sizeof(header_entry));
+  header->entry->key = "Content-Type";
+  header->entry->value = get_content_type(req->path);
+  if ((header->entry->value) == NULL)
+  {
+    return 422;
+  }
+  header = (header_list*)malloc(sizeof(header_list));
+  resp->headers->next = header;
+  header->entry = (header_entry*)malloc(sizeof(header_entry));
+  header->entry->key = "Content-Length";
+  header->entry->value = get_content_length(resp->body_fd);
+  print_headers(resp->headers, "");
+  if ((sprintf(buf, "HTTP/1.1 200 OK\n%s: %s\n%s: %s\n\n",
+    resp->headers->entry->key, resp->headers->entry->value,
+    resp->headers->next->entry->key, resp->headers->next->entry->value
+  )) < 0)
+  {
+    perror("something's wrong");
+    return 1;
+  };
+  if ((write(client_sock, buf, strlen(buf))) == -1)
+  {
+    perror("writing to client socket");
+    return 1;
+  }
+
+  ssize_t num_bytes;
+  while ((num_bytes = fread(buf, sizeof(char), BUF_SIZE, resp->body_fd)) != 0)
+  {
+    if (num_bytes == -1){
+      perror("error reading response file");
+      return 1;
+    }
+    write(client_sock, buf, num_bytes);
+  }
+  return 0;
+}
+
+// write_http_error can be called when processing a given request fails
+// before beginning to write the response. It simply returns the first
+// line of the HTTP response and closes the connection
+int write_http_error(int client_socket, int status_code)
+{
+  return 0;
+}
+
+// Callers are responsible for freeing the returned char* returned by get_content_type
+char* get_content_type(char* path)
+{
+  char* content_type = (char*)malloc(sizeof(char) * 256); // We don't really expect the header to be this large 
+  const char* extension = strrchr(path, '.');
+  if (!extension)
+  {
+    return NULL;
+  }
+  if ((strncmp(extension, ".html", sizeof(char) * 6)) == 0)
+  {
+    content_type = "text/html";
+  } else if ((strncmp(extension, ".jpg", sizeof(char) * 5)) == 0)
+  {
+    content_type = "image/jpeg";
+  } else if ((strncmp(extension, ".jpeg", sizeof(char) * 6)) == 0)
+  {
+    content_type = "image/jpeg";
+  } else if ((strncmp(extension, ".mp3", sizeof(char) * 5)) == 0)
+  {
+    content_type = "audio/mpeg";
+  } else if ((strncmp(extension, ".mp4", sizeof(char) * 5)) == 0)
+  {
+    content_type = "video/mp4";
+  } else if ((strncmp(extension, ".ico", sizeof(char) * 5)) == 0) {
+    printf("I AM HERE\n");
+    content_type = "image/x-icon";
+  } else {
+    content_type = NULL;
+  }
+  return content_type;
+}
+
+// get_content_length returns the length (in bytes) of file
+char* get_content_length(FILE* file)
+{
+  char* buf = (char*)malloc(sizeof(char)*BUF_SIZE);
+  fseek(file, 0, SEEK_END);
+  long content_length = ftell(file);
+  fseek(file, -content_length, SEEK_END);
+  sprintf(buf, "%ld", content_length);
+  return buf;
 }
 
 // print_headers is a utility for easily printing out all headers of a header_list*
@@ -118,84 +248,4 @@ void print_headers(header_list* headers, char* prefix)
     printf("%s%s: %s\n", prefix, current->entry->key, current->entry->value);
     current = current->next;
   }
-}
-
-int serve_request(int client_sock, http_req* req, http_resp* resp)
-{
-  char* local_path = (char*)malloc(strlen(WEB_DIR) + strlen(req->path) + 1); // +1 for \0 byte
-  local_path = strncat(WEB_DIR, req->path, strlen(req->path));
-  // Check path exists
-  struct stat st;
-  if ((stat(local_path, &st)) == -1)
-  {
-    // For simplicity we assume any failure is ENOENT and we'll return 404
-    perror("error calling stat on request path");
-    free(local_path);
-    // set 404 resp
-    return 1;
-  }
-  resp->body_fd = fopen(local_path, "r");
-  if (resp->body_fd == NULL)
-  {
-    perror("error opening requested file");
-    free(local_path);
-    // set 500 resp
-    return 1;
-  }
-  free(local_path);
-  // We are ready to send
-  resp->status_code = 200;
-  resp->headers = (header_list*)malloc(sizeof(header_list));
-  resp->headers->entry = (header_entry*)malloc(sizeof(header_entry));
-  resp->headers->entry->key = "Content-Type";
-  resp->headers->entry->value = get_content_type(req->path);
-  if ((resp->headers->entry->value) == NULL)
-  {
-    // Bad request
-    return 1;
-  }
-  char* buf[BUF_SIZE];
-  
-  FILE* resp_body_buf = fmemopen(buf, BUF_SIZE, "w+");
-  if (resp_body_buf == NULL)
-  {
-    perror("fmemopen for response body buffer");
-  }
-  dprintf(client_sock, "HTTP/1.1 200 OK\n%s: %s\n\n", resp->headers->entry->key, resp->headers->entry->value);  // Slight hack: we're only using content-type header for now
-  ssize_t num_bytes;
-  while ((num_bytes == read(resp->body_fd->_fileno, &buf, sizeof(buf))) != 0)
-  {
-    write(client_sock, &buf, num_bytes);
-  }
-  return 0;
-}
-
-// Callers are responsible for freeing the returned char* returned by get_content_type
-char* get_content_type(char* path)
-{
-  char* content_type = (char*)malloc(sizeof(char) * 256); // We don't really expect the header to be this large 
-  const char* extension = strrchr(path, '.');
-  if (!extension)
-  {
-    return NULL;
-  }
-  if ((strncmp(extension, "html", sizeof(char) * 4)) == 0)
-  {
-    content_type = "text/html";
-  } else if ((strncmp(extension, "jpg", sizeof(char) * 3))== 0)
-  {
-    content_type = "image/jpeg";
-  } else if ((strncmp(extension, "jpeg", sizeof(char) * 4))== 0)
-  {
-    content_type = "image/jpeg";
-  } else if ((strncmp(extension, "mp3", sizeof(char) * 3))== 0)
-  {
-    content_type = "audio/mpeg";
-  } else if ((strncmp(extension, "mp4", sizeof(char) * 3))== 0)
-  {
-    content_type = "video/mp4";
-  } else {
-    content_type = NULL;
-  }
-  return content_type;
 }
--- request_handler.h
+++ request_handler.h
@@ -49,6 +49,13 @@ int parse_http_req(char* buffer, size_t buf_len, http_req* req);
 // encapsulated in req. The return value is the HTTP status code to be used in the response
 int serve_request(int client_sock, http_req* req, http_resp* resp);
 
+// write_http_error can be called when processing a given request fails
+// before beginning to write the response. It simply returns the first
+// line of the HTTP response and closes the connection
+int write_http_error(int client_sock, int status_code);
+
 void print_headers(header_list* headers, char* prefix);
 
 char* get_content_type(char* path);
+
+char* get_content_length(FILE* file);
