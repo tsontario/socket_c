3c954f7bb9bc74b3965e946b02a3d60c4104ccd7
Timothy Smith <tsontario@gmail.com>
2019-10-27 00:10:35 -0400

    name change + make clean before container build

--- scripts/build_container.sh
+++ scripts/build_container.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Run this from project root
+make clean
 docker build . -t csi4118:1.1
--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -td --net=host csi4118:1.1 ./server -p8989
+docker run -td --net=host csi4118:1.1 ./myServer -p8989
